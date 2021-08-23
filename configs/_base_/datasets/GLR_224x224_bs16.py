1e4dc70e7e650ca2ef5a93b502bb2cdb6209a8ff
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-08-23 11:39:04 +0800

    Update GLR_224x224_bs16.py

--- configs/_base_/datasets/GLR_224x224_bs16.py
+++ configs/_base_/datasets/GLR_224x224_bs16.py
@@ -9,8 +9,8 @@ train_pipeline = [
     dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='ImageToTensor', keys=['img']),
-    dict(type='ToTensor', keys=['gt_label']),
-    dict(type='Collect', keys=['img', 'gt_label'])
+    # dict(type='ToTensor', keys=['gt_label']),
+    # dict(type='Collect', keys=['img', 'gt_label'])
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
