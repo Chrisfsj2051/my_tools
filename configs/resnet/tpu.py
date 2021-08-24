b3be8f2e8b330d52dbe8e70938092f246fc2b0cc
Chrisfsj2051 <Chrisfsj2051@gmail.com>
2021-08-24 10:24:38 +0800

    Update tpu.py

--- configs/resnet/tpu.py
+++ configs/resnet/tpu.py
@@ -1,12 +1,19 @@
 _base_ = [
     '../_base_/models/resnet152.py', '../_base_/datasets/GLR_224x224_bs16.py',
-    '../_base_/schedules/GLR_bs16_10e.py', '../_base_/default_runtime.py'
+    '../_base_/schedules/GLR_bs16_20k.py', '../_base_/default_runtime.py'
 ]
 
-# fp16 = dict(loss_scale='dynamic')
-# resume_from = 'work_dirs/debug/iter_520000.pth'
-# evaluation = dict(interval=10000, metric='accuracy')
-data = dict(workers_per_gpu=4, samples_per_gpu=128)
+fp16 = dict(loss_scale='dynamic')
+# yapf:disable
+log_config = dict(
+    interval=1000,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+    ])
+checkpoint_config = dict(interval=10000)
+evaluation = dict(interval=20000, metric='accuracy')
+data = dict(workers_per_gpu=4, samples_per_gpu=64)
 
 # model settings
 model = dict(
@@ -19,11 +26,3 @@ model = dict(
         num_classes=81313
     )
 )
-
-# total_iters = 2000*1000
-# # optimizer
-# optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001)
-# optimizer_config = dict(grad_clip=None)
-# # learning policy
-# lr_config = dict(policy='step', step=[total_iters//2, 3 * total_iters//4], by_epoch=False)
-# runner = dict(type='IterBasedRunner', max_iters=total_iters)
